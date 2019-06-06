Correct calculation of coordinates

src/beam.py
@@ -1,8 +1,11 @@
 import numpy as np
 <<<<<<< HEAD
+<<<<<<< HEAD
 import matplotlib.pyplot as plt
 =======
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
 from scipy.optimize import least_squares
 from photutils import find_peaks
 from astropy.stats import sigma_clipped_stats
@@ -131,11 +134,15 @@ class beam(object):
             fit_param, var = self.fit()
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             fit_data = self.multivariate_gaussian_2d(fit_param.x)\
                             .reshape(np.outer(self.xgrid, self.ygrid).shape)
 =======
             fit_data = self.multivariate_gaussian_2d(fit_param.x).reshape(np.outer(self.xgrid, self.ygrid).shape)
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
+=======
+            fit_data = self.multivariate_gaussian_2d(fit_param.x).reshape(np.outer(self.xgrid, self.ygrid).shape)
+>>>>>>> 8989c24... Correct calculation of coordinates
             res = self.data-fit_data
 
             self.peak_finder(map_data=res)
@@ -160,12 +167,18 @@ class computeoffset():
     def centroid(self, threshold=0.275):
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
         '''
         For more information about centroid calculation see Shariff, PhD Thesis, 2016
         '''
 
+<<<<<<< HEAD
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
         maxval = np.max(self.data)
         minval = np.min(self.data)
         y_max, x_max = np.where(self.data == maxval)
@@ -236,6 +249,7 @@ class computeoffset():
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             print
 >>>>>>> 4ee3dbe... Fixed bug in selecting data
@@ -248,15 +262,19 @@ class computeoffset():
             return offset_angle[0].degree, offset_angle[1].degree
 
             # if self.ctype == 'AZ and EL':
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
 
-            #     return offset_angle.az, offset_angle.alt
+            if self.ctype == 'RA and DEC':
+                coord = wcs.utils.pixel_to_skycoord(x_c, y_c, wcs_trans)
+                offset_angle = coord_centre.spherical_offsets_to(coord)
 
-            # elif self.ctype == 'RA and DEC':
-            #     print('ANGLE', offset_angle[0].degree)
-            #     return offset_angle.ra, offset_angle.dec
+                return offset_angle[0].degree, offset_angle[1].degree
 
-            # elif self.ctype == 'CROSS-EL and EL':
+            elif self.ctype == 'CROSS-EL and EL':
+                coord = wcs_trans.wcs_pix2world(x_c, y_c, 1.)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
                 return offset_angle.az*np.cos(offset_angle.alt), offset_angle.alt
 =======
@@ -275,6 +293,9 @@ class computeoffset():
 =======
             #     return offset_angle.az*np.cos(offset_angle.alt), offset_angle.alt
 >>>>>>> ccf3a8f... Added output for offset calculation
+=======
+                return coord[0]-self.angX_center, coord[1]-self.angY_center
+>>>>>>> 8989c24... Correct calculation of coordinates
 
 
 

src/gui.py
@@ -11,6 +11,7 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> fbc01b1... Gui updated with polarizarion maps tabs
 from functools import partial
 =======
@@ -20,6 +21,9 @@ from PyQt5.QtGui import *
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
 =======
 >>>>>>> 4ee3dbe... Fixed bug in selecting data
+=======
+from PyQt5.QtGui import *
+>>>>>>> 8989c24... Correct calculation of coordinates
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -66,7 +70,7 @@ class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'Naive MapMaker'
+        self.title = 'NaMap'
 
         self.setWindowTitle(self.title)
         
@@ -131,21 +135,13 @@ class MainWindowTab(QTabWidget):
 
         #functions to compute the updated values
         self.tab1.load_func()
-        process = psutil.Process(os.getpid())
-        print('MEM0',process.memory_info().rss/1e9)
 
         self.data = self.tab1.detslice 
 
         self.cleandata = self.tab1.cleaned_data
 
-        process = psutil.Process(os.getpid())
-        print('MEM1',process.memory_info().rss/1e9)
-
         self.tab2.draw_TOD(self.data)
         self.tab2.draw_cleaned_TOD(self.cleandata)
-
-        process = psutil.Process(os.getpid())
-        print('MEM2',process.memory_info().rss/1e9)
 
         try:
             self.tab1.mapvalues(self.cleandata)
@@ -165,9 +161,6 @@ class MainWindowTab(QTabWidget):
         except AttributeError:
             pass
 
-        process = psutil.Process(os.getpid())
-        print('MEM3',process.memory_info().rss/1e9)
-        print('END CYCLE')
     
     def save2fits(self): #function to save the map as a FITS file
         hdr = self.tab1.proj.to_header() #grabs the projection information for header
@@ -1256,28 +1249,15 @@ class ParamMapTab(QWidget):
                                          self.coord1type.text(), self.coord2type.text(), \
                                          self.experiment.currentText())
 
-                process = psutil.Process(os.getpid())
-                print('MEM01_LOAD',process.memory_info().rss/1e9)
-
                 self.det_data, self.coord1_data, self.coord2_data = dataload.values()
-
-                process = psutil.Process(os.getpid())
-                print('MEM015_LOAD',process.memory_info().rss/1e9)
 
                 pickle.dump(self.det_data, open(det_path_pickle, 'wb'))  
                 pickle.dump(self.coord1_data, open(coord1_path_pickle, 'wb'))
                 pickle.dump(self.coord2_data, open(coord2_path_pickle, 'wb'))
 
-                process = psutil.Process(os.getpid())
-                print('MEM02_LOAD',process.memory_info().rss/1e9)
-
                 del dataload
                 gc.collect()
-
-            process = psutil.Process(os.getpid())
-            print('MEM01',process.memory_info().rss/1e9)
             
-
             if self.experiment.currentText().lower() == 'blast-tng':
                 zoomsyncdata = ld.frame_zoom_sync(self.det_data, self.detfreq.text(), \
                                                   self.detframe.text(), self.coord1_data, \
@@ -1293,17 +1273,22 @@ class ParamMapTab(QWidget):
                                                   self.acsframe.text(), self.startframe.text(), \
                                                   self.endframe.text(), self.experiment.currentText())
 
-            process = psutil.Process(os.getpid())
-            print('MEM02',process.memory_info().rss/1e9)
-
             (self.timemap, self.detslice, self.coord1slice, \
              self.coord2slice) = zoomsyncdata.sync_data()
 
             if self.DirConvCheckBox.isChecked:
                 self.dirfile_conversion()
 
-            process = psutil.Process(os.getpid())
-            print('MEM01_CONV',process.memory_info().rss/1e9)
+            ### CONVERSION TO RADIANS FOR ALL THE ANGLES ###
+
+            self.coord2slice = np.radians(self.coord2slice)
+
+            if self.coord1.lower() == 'ra':
+                self.coord1slice = np.radians(self.coord1slice*15.) #Conversion between hours to degree and then converted to radians
+            elif self.coord1.lower() == 'cross-el':
+                self.coord1slice = np.radians(self.coord1slice)*np.cos(self.coord2slice)
+            else:
+                self.coord1slice = np.radians(self.coord1slice)
             
             del self.det_data
             del self.coord1_data
@@ -1311,15 +1296,7 @@ class ParamMapTab(QWidget):
             del zoomsyncdata
             gc.collect()
 
-            process = psutil.Process(os.getpid())
-            print('MEM03',process.memory_info().rss/1e9)
-
             self.clean_func()
-
-            process = psutil.Process(os.getpid())
-            print('MEM04',process.memory_info().rss/1e9)
-
-            print('DataLoaded')
 
     def clean_func(self):
 
@@ -1342,9 +1319,6 @@ class ParamMapTab(QWidget):
                                          float(self.bcoord1conv.text()))
         coord2_conv = ld.convert_dirfile(self.coord2slice, float(self.acoord2conv.text()), \
                                          float(self.bcoord2conv.text()))
-                                         
-        process = psutil.Process(os.getpid())
-        print('MEM04',process.memory_info().rss/1e9)
 
         det_conv.conversion()
         coord1_conv.conversion()

src/mapmaker.py
@@ -1,10 +1,12 @@
 import numpy as np
 import copy
 <<<<<<< HEAD
+<<<<<<< HEAD
 from astropy import wcs, coordinates
+=======
+from astropy import wcs
+>>>>>>> 8989c24... Correct calculation of coordinates
 from astropy.convolution import Gaussian2DKernel, convolve
-import matplotlib.pyplot as plt
-
 
 class maps():
 
@@ -19,8 +21,8 @@ class maps():
         self.crpix = crpix             #see wcs_world for explanation of this parameter
         self.cdelt = cdelt             #see wcs_world for explanation of this parameter
         self.crval = crval             #see wcs_world for explanation of this parameter
-        self.coord1 = coord1           #array of the first coordinate
-        self.coord2 = coord2           #array of the second coordinate
+        self.coord1 = np.degrees(coord1)          #array of the first coordinate
+        self.coord2 = np.degrees(coord2)           #array of the second coordinate
         self.data = data               #cleaned TOD that is used to create a map
         self.w = 0.                    #initialization of the coordinates of the map in pixel coordinates
         self.proj = 0.                 #inizialization of the wcs of the map. see wcs_world for more explanation about projections
@@ -89,7 +91,9 @@ class maps():
             if not self.convolution:
                 return Imap
             else:
-                return mapmaker.convolution(self.std, Imap)
+                std_pixel = self.std/3600./self.cdelt[0]
+                
+                return mapmaker.convolution(std_pixel, Imap)
         else:        
             Imap, Qmap, Umap = mapmaker.map_singledetector(self.crpix)
             if not self.convolution:
@@ -210,17 +214,23 @@ class wcs_world():
 =======
         if self.ctype == 'RA and DEC':
             w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+<<<<<<< HEAD
         elif self.ctype == 'AZ and EL' or self.ctype == 'CROSS-EL and EL':
 >>>>>>> 4ee3dbe... Fixed bug in selecting data
             w.wcs.ctype = ["TLON-ARC", "TLAT-ARC"]
 =======
         if self.ctype == 'RA and DEC':
             w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
         elif self.ctype == 'AZ and EL':
             w.wcs.ctype = ["TLON-ARC", "TLAT-ARC"]
         elif self.ctype == 'CROSS-EL and EL':
             w.wcs.ctype = ["TLON-CAR", "TLAT-CAR"]
+<<<<<<< HEAD
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
+=======
+>>>>>>> 8989c24... Correct calculation of coordinates
         world = w.wcs_world2pix(coord, 1)
 
         return world, w
@@ -296,12 +306,6 @@ class mapmaking(object):
         
         x_map = self.pixelmap[:,0]   #RA 
         y_map = self.pixelmap[:,1]   #DEC
-
-        # print('Coordinates in X and Y')
-        # print(x_map)
-        # print(y_map)
-        # print(np.amax(x_map),np.amin(x_map))
-        # print(np.amin(y_map),np.amax(y_map))
         
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -612,10 +616,14 @@ class mapmaking(object):
 
             mapvalues = self.map_singledetector(value=self.data[i],sigma=self.weight[i],\
 <<<<<<< HEAD
+<<<<<<< HEAD
                                            angle=self.polangle[i])
 =======
                                                 angle=self.polangle[i])
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
+=======
+                                                angle=self.polangle[i])
+>>>>>>> 8989c24... Correct calculation of coordinates
             
             if i == 0:
                 I_map = mapvalues[0].copy()
@@ -636,9 +644,7 @@ class mapmaking(object):
 =======
         '''
         Function to convolve the maps with a gaussian.
-        STD is in pixel values (20190531 from GC std right now is in arcsed 
-        according to the label, so the conversion between arcsec to pixel needs 
-        to be created)
+        STD is in pixel values
         '''
 >>>>>>> 651e1e6... Commented files
 