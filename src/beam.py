Correct calculation of coordinates

src/beam.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
 from photutils import find_peaks
 from astropy.stats import sigma_clipped_stats
@@ -104,8 +103,7 @@ class beam(object):
 
             fit_param, var = self.fit()
 
-            fit_data = self.multivariate_gaussian_2d(fit_param.x)\
-                            .reshape(np.outer(self.xgrid, self.ygrid).shape)
+            fit_data = self.multivariate_gaussian_2d(fit_param.x).reshape(np.outer(self.xgrid, self.ygrid).shape)
             res = self.data-fit_data
 
             self.peak_finder(map_data=res)
@@ -128,6 +126,10 @@ class computeoffset():
         self.ctype = ctype
 
     def centroid(self, threshold=0.275):
+
+        '''
+        For more information about centroid calculation see Shariff, PhD Thesis, 2016
+        '''
 
         maxval = np.max(self.data)
         minval = np.min(self.data)
@@ -165,23 +167,17 @@ class computeoffset():
             return x_off, y_off
         
         else:
-            coord = wcs.utils.pixel_to_skycoord(x_c, y_c, wcs_trans)
-                        
-            offset_angle = coord_centre.spherical_offsets_to(coord)
 
-            return offset_angle[0].degree, offset_angle[1].degree
+            if self.ctype == 'RA and DEC':
+                coord = wcs.utils.pixel_to_skycoord(x_c, y_c, wcs_trans)
+                offset_angle = coord_centre.spherical_offsets_to(coord)
 
-            # if self.ctype == 'AZ and EL':
+                return offset_angle[0].degree, offset_angle[1].degree
 
-            #     return offset_angle.az, offset_angle.alt
+            elif self.ctype == 'CROSS-EL and EL':
+                coord = wcs_trans.wcs_pix2world(x_c, y_c, 1.)
 
-            # elif self.ctype == 'RA and DEC':
-            #     print('ANGLE', offset_angle[0].degree)
-            #     return offset_angle.ra, offset_angle.dec
-
-            # elif self.ctype == 'CROSS-EL and EL':
-
-            #     return offset_angle.az*np.cos(offset_angle.alt), offset_angle.alt
+                return coord[0]-self.angX_center, coord[1]-self.angY_center
 
 
 

src/loaddata.py
@@ -87,14 +87,11 @@ class data_value():
         det_data = self.load(self.det_path, self.det_name, self.det_file_type)
         coord2_data = self.load(self.coord_path, self.coord2_name.lower(), self.coord2_file_type)
 
-        if self.coord1_name.lower() == 'cross-el':
-            coord1_data = self.load(self.coord_path, 'az', self.coord1_file_type)
-            coord1_data = coord1_data*np.cos(coord2_data)
-        elif self.coord1_name.lower() == 'ra':
+        if self.coord1_name.lower() == 'ra':
             coord1_data = self.load(self.coord_path, self.coord1_name.lower(), self.coord1_file_type)
-            coord1_data = coord1_data*15.
         else:
-            coord1_data = self.load(self.coord_path, self.coord1_name.lower(), self.coord1_file_type)
+            coord1_data = self.load(self.coord_path, 'az', self.coord1_file_type)
+            
         return det_data, coord1_data, coord2_data
 
 class convert_dirfile():

src/mapmaker.py
@@ -1,9 +1,7 @@
 import numpy as np
 import copy
-from astropy import wcs, coordinates
+from astropy import wcs
 from astropy.convolution import Gaussian2DKernel, convolve
-import matplotlib.pyplot as plt
-
 
 class maps():
 
@@ -18,8 +16,8 @@ class maps():
         self.crpix = crpix             #see wcs_world for explanation of this parameter
         self.cdelt = cdelt             #see wcs_world for explanation of this parameter
         self.crval = crval             #see wcs_world for explanation of this parameter
-        self.coord1 = coord1           #array of the first coordinate
-        self.coord2 = coord2           #array of the second coordinate
+        self.coord1 = np.degrees(coord1)          #array of the first coordinate
+        self.coord2 = np.degrees(coord2)           #array of the second coordinate
         self.data = data               #cleaned TOD that is used to create a map
         self.w = 0.                    #initialization of the coordinates of the map in pixel coordinates
         self.proj = 0.                 #inizialization of the wcs of the map. see wcs_world for more explanation about projections
@@ -50,7 +48,9 @@ class maps():
             if not self.convolution:
                 return Imap
             else:
-                return mapmaker.convolution(self.std, Imap)
+                std_pixel = self.std/3600./self.cdelt[0]
+                
+                return mapmaker.convolution(std_pixel, Imap)
         else:        
             Imap, Qmap, Umap = mapmaker.map_singledetector(self.crpix)
             if not self.convolution:
@@ -103,8 +103,10 @@ class wcs_world():
         w.wcs.crval = self.crval
         if self.ctype == 'RA and DEC':
             w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
-        elif self.ctype == 'AZ and EL' or self.ctype == 'CROSS-EL and EL':
+        elif self.ctype == 'AZ and EL':
             w.wcs.ctype = ["TLON-ARC", "TLAT-ARC"]
+        elif self.ctype == 'CROSS-EL and EL':
+            w.wcs.ctype = ["TLON-CAR", "TLAT-CAR"]
         world = w.wcs_world2pix(coord, 1)
 
         return world, w
@@ -148,12 +150,6 @@ class mapmaking(object):
         
         x_map = self.pixelmap[:,0]   #RA 
         y_map = self.pixelmap[:,1]   #DEC
-
-        # print('Coordinates in X and Y')
-        # print(x_map)
-        # print(y_map)
-        # print(np.amax(x_map),np.amin(x_map))
-        # print(np.amin(y_map),np.amax(y_map))
         
         if (np.amin(x_map)) <= 0:
             x_map = np.floor(x_map+np.abs(np.amin(x_map)))
@@ -285,7 +281,7 @@ class mapmaking(object):
         for i in range(self.number):
 
             mapvalues = self.map_singledetector(value=self.data[i],sigma=self.weight[i],\
-                                           angle=self.polangle[i])
+                                                angle=self.polangle[i])
             
             if i == 0:
                 I_map = mapvalues[0].copy()
@@ -302,9 +298,7 @@ class mapmaking(object):
 
         '''
         Function to convolve the maps with a gaussian.
-        STD is in pixel values (20190531 from GC std right now is in arcsed 
-        according to the label, so the conversion between arcsec to pixel needs 
-        to be created)
+        STD is in pixel values
         '''
 
         kernel = Gaussian2DKernel(x_stddev=std)