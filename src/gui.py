dfc2e45f2f4dad4561077769f33b23e5bf1f056e
gabrielecoppi <gabriele@MSI.localdomain>
2019-07-19 08:54:11 -0400

    Correct pointing offset calculation

--- src/gui.py
+++ src/gui.py
@@ -1267,20 +1267,17 @@ class ParamMapTab(QWidget):
                 self.lstslice = None
                 self.latslice = None
 
-            print('BEFORE', self.coord2slice)
-
             if self.DirConvCheckBox.isChecked:
                 self.dirfile_conversion(correction = correction, LSTconv = LSTconv, \
                                         LATconv = LATconv)
             
-            print('AFTER', self.coord2slice)
-            if self.coord1.lower() == 'ra':
-                self.coord1slice = self.coord1slice*15. #Conversion between hours to degree
-            elif self.coord1.lower() == 'cross-el':
+            print('AFTER', self.coord1slice, self.coord2slice)
+            
+            if self.coord1.lower() == 'cross-el':
                 self.coord1slice = self.coord1slice*np.cos(np.radians(self.coord2slice))
-            else:
-                self.coord1slice = self.coord1slice
-
+            # else:
+            #     self.coord1slice = self.coord1slice
+            
             if correction is True:
 
                 xsc_file = ld.xsc_offset(self.pointingoffsetnumber.text(), self.startframe.text(), self.endframe.text())
@@ -1291,7 +1288,9 @@ class ParamMapTab(QWidget):
                                        xsc_offset, lst = self.lstslice, lat = self.latslice)
 
                 self.coord1slice, self.coord2slice = corr.correction()
-
+            else:
+                if self.coord1.lower() == 'ra':
+                    self.coord1slice = self.coord1slice*15. #Conversion between hours to degree
            
             
             del self.det_data
