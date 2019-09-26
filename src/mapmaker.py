Correct telescope coordinates calculation

src/mapmaker.py
@@ -10,7 +10,6 @@ from astropy import wcs, coordinates
 from astropy import wcs
 >>>>>>> 8989c24... Correct calculation of coordinates
 from astropy.convolution import Gaussian2DKernel, convolve
-import matplotlib.pyplot as plt
 
 class maps():
 
@@ -19,7 +18,8 @@ class maps():
     In this way in the gui.py only one class is called
     '''
 
-    def __init__(self, ctype, crpix, cdelt, crval, data, coord1, coord2, convolution, std, Ionly=True, pol_angle=0.,noise=1.):
+    def __init__(self, ctype, crpix, cdelt, crval, data, coord1, coord2, convolution, std, Ionly=True, pol_angle=0.,noise=1., \
+                 telcoord=False, parang=None):
 
         self.ctype = ctype             #see wcs_world for explanation of this parameter
         self.crpix = crpix             #see wcs_world for explanation of this parameter
@@ -35,6 +35,11 @@ class maps():
         self.Ionly = Ionly             #paramters to check if only I is required to be computed
         self.pol_angle = pol_angle     #polariztion angle
         self.noise = noise             #white level noise of detector(s)
+        self.telcoord = telcoord       #If True the map is drawn in telescope coordinates. That means that the projected plane is rotated
+        if parang is not None:
+            self.parang = np.radians(parang)   #Parallactic Angle. This is used to compute the pixel indices in telescopes coordinates
+        else:
+            self.parang = parang
 
     def wcs_proj(self):
 <<<<<<< HEAD
@@ -73,6 +78,7 @@ class maps():
         '''
         Function to compute the projection and the pixel coordinates
         '''
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
@@ -81,23 +87,28 @@ class maps():
 =======
 >>>>>>> 651e1e6... Commented files
         wcsworld = wcs_world(self.ctype, self.crpix, self.cdelt, self.crval)
+=======
+        print('IN')
+        wcsworld = wcs_world(self.ctype, self.crpix, self.cdelt, self.crval, self.telcoord, self.parang)
+>>>>>>> 59060e4... Correct telescope coordinates calculation
 
         if np.size(np.shape(self.data)) == 1:
-            try:
-                self.w, self.proj = wcsworld.world(np.transpose(np.array([self.coord1[0], self.coord2[0]])))
-            except RuntimeError:
-                self.w, self.proj = wcsworld.world(np.transpose(np.array([self.coord1, self.coord2])))
-        else:
 
+            if np.size(np.shape(self.coord1)) != 1:
+                coord_array = np.transpose(np.array([self.coord1[0], self.coord2[0]]))
+            else:
+                coord_array = np.transpose(np.array([self.coord1, self.coord2]))
+            try:
+                self.w, self.proj = wcsworld.world(coord_array)
+            except RuntimeError:
+                self.w, self.proj = wcsworld.world(coord_array)
+        else:
             if np.size(np.shape(self.coord1)) == 1:
                 self.w, self.proj = wcsworld.world(np.transpose(np.array([self.coord1, self.coord2])))
             else:
                 self.w = np.zeros((np.size(np.shape(self.data)), len(self.coord1[0]), 2))
                 for i in range(np.size(np.shape(self.data))):
                     self.w[i,:,:], self.proj = wcsworld.world(np.transpose(np.array([self.coord1[i], self.coord2[i]])))
-                    plt.plot(self.coord1[i])
-
-                plt.show()
 
     def map2d(self):
 
@@ -204,7 +215,7 @@ class maps():
                     
                     return mapmaker.convolution(std_pixel, Imap)
             else:        
-                Imap, Qmap, Umap = mapmaker.map_singledetector(self.crpix)
+                Imap, Qmap, Umap = mapmaker.map_multidetectors(self.crpix)
                 if not self.convolution:
                     return Imap, Qmap, Umap
                 else:
@@ -223,13 +234,19 @@ class wcs_world():
     Class to generate a wcs using astropy routines.
     '''
 
+<<<<<<< HEAD
 >>>>>>> 651e1e6... Commented files
     def __init__(self, ctype, crpix, crdelt, crval):
+=======
+    def __init__(self, ctype, crpix, crdelt, crval, telcoord=False, parang=None):
+>>>>>>> 59060e4... Correct telescope coordinates calculation
 
         self.ctype = ctype    #ctype of the map, which projection is used to convert coordinates to pixel numbers
         self.crdelt = crdelt  #cdelt of the map, distance in deg between two close pixels
         self.crpix = crpix    #crpix of the map, central pixel of the map in pixel coordinates
         self.crval = crval    #crval of the map, central pixel of the map in sky/telescope (depending on the system) coordinates
+        self.telcoord = telcoord #Telescope coordinates boolean value. Check map class for more explanation
+        self.parang = parang  #Parallactic angle. Check map class for more explanation
 
     def world(self, coord):
         
@@ -263,6 +280,7 @@ class wcs_world():
         w.wcs.crpix = self.crpix
         w.wcs.cdelt = self.crdelt
         w.wcs.crval = self.crval
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         if self.ctype.lower() == 'RA and DEC':
@@ -299,7 +317,30 @@ class wcs_world():
         #f.close()
         #print('printing coords')
         #print(coord)
+=======
+>>>>>>> 59060e4... Correct telescope coordinates calculation
 
+        if self.telcoord is False:
+            if self.ctype == 'RA and DEC':
+                w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+            elif self.ctype == 'AZ and EL':
+                w.wcs.ctype = ["TLON-ARC", "TLAT-ARC"]
+            elif self.ctype == 'CROSS-EL and EL':
+                w.wcs.ctype = ["TLON-CAR", "TLAT-CAR"]
+            print('WORLD_BUILT', coord)
+            world = w.all_world2pix(coord, 1)
+            
+        else:
+            w.wcs.ctype = ["TLON-TAN", "TLAT-TAN"]
+            world = np.zeros_like(coord)
+            px = w.wcs.s2p(coord, 1)
+            print('COORD', coord)
+            print('PIXEL', px['imgcrd'][:,0])
+            #Use the parallactic angle to rotate the projected plane
+            world[:,0] = (px['imgcrd'][:,0]*np.cos(self.parang)-px['imgcrd'][:,1]*np.sin(self.parang))/self.crdelt[0]+self.crpix[0]
+            world[:,1] = (px['imgcrd'][:,0]*np.sin(self.parang)+px['imgcrd'][:,1]*np.cos(self.parang))/self.crdelt[1]+self.crpix[1]
+        
+        print('WORLD', world, np.shape(world))
         return world, w
 
 class mapmaking(object):
@@ -445,6 +486,7 @@ class mapmaking(object):
         m_flat = np.bincount(param, weights=0.5*cos*sin)*sigma
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 <<<<<<< HEAD
         Delta = c_flat**2*(c2_flat-N_hits_flat)+2*s_flat*c_flat*m_flat-c2_flat*s_flat**2-\
@@ -465,8 +507,10 @@ class mapmaking(object):
         D = -((c2_flat-N_hits_flat)*N_hits_flat+s_flat**2)
         E = c_flat*s_flat-m_flat*N_hits_flat
         F = c2_flat*N_hits_flat-c_flat**2
+=======
+>>>>>>> 59060e4... Correct telescope coordinates calculation
 
-        return I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, Delta, A, B, C, D, E, F, param
+        return I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, c_flat, c2_flat, s_flat, s2_flat, m_flat, param
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -621,10 +665,10 @@ class mapmaking(object):
                 idxpixel = self.pixelmap[i].copy()
             print('Pixel_MIN', np.amin(idxpixel[:,0]), np.amin(idxpixel[:,1]))
             print('Pixel_MIN', np.amax(idxpixel[:,0]), np.amax(idxpixel[:,1]))
-            # mapvalues = self.map_singledetector_Ionly(crpix = crpix, value=self.data[i],noise=1/self.weight[i]**2,\
+            # mapvalues = self.map_singledetector_Ionly(crpix = crpix, value=self.data[i],noise=1/self.weight[i],\
             #                                           angle=self.polangle[i], idxpixel = idxpixel)
 
-            value = self.map_param(crpix=crpix, idxpixel = idxpixel, value=self.data[i], noise=1/self.weight[i]**2, angle=self.polangle[i])
+            value = self.map_param(crpix=crpix, idxpixel = idxpixel, value=self.data[i], noise=1/self.weight[i], angle=self.polangle[i])
 
             num_temp_flat = np.zeros(len(value[0]))
             num_temp_flat[np.nonzero(value[0])] = value[0][np.nonzero(value[0])]
@@ -695,9 +739,18 @@ class mapmaking(object):
         else:
             idxpixel = idxpixel
 
-        (I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, Delta, \
-         A, B, C, D, E, F, param) = self.map_param(crpix=crpix, idxpixel=idxpixel, value=value, \
-                                                   sigma=1/self.weight**2,angle=self.polangle)
+        (I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, \
+         c_flat, c2_flat, s_flat, s2_flat, m_flat, param) = self.map_param(crpix=crpix, idxpixel=idxpixel, value=value, \
+                                                                           noise=1/self.weight**2,angle=self.polangle)
+
+        Delta = (c_flat**2*(c2_flat-N_hits_flat)+2*s_flat*c_flat*m_flat-c2_flat*s_flat**2-\
+                 N_hits_flat*(c2_flat**2+m_flat**2-c2_flat*N_hits_flat))
+        A = -(c2_flat**2+m_flat**2-c2_flat*N_hits_flat)
+        B = c_flat*(c2_flat-N_hits_flat)+s_flat*m_flat
+        C = c_flat*m_flat-s_flat*c2_flat
+        D = -((c2_flat-N_hits_flat)*N_hits_flat+s_flat**2)
+        E = c_flat*s_flat-m_flat*N_hits_flat
+        F = c2_flat*N_hits_flat-c_flat**2
 
 =======
 >>>>>>> 6c0d8b1... Solved some errors in polarization maps (still some to be corrected)
@@ -787,7 +840,7 @@ class mapmaking(object):
 >>>>>>> c2f9e18a58705b8f7b3979aa1ee2eb19c9939d72
 =======
         ind_pol, = np.nonzero(Q_pixel_flat)
-        pol = np.sqrt(Q_pixel_flat**2+U_pixel_flat**2)
+        #pol = np.sqrt(Q_pixel_flat**2+U_pixel_flat**2)
 
 >>>>>>> 6c0d8b1... Solved some errors in polarization maps (still some to be corrected)
         I_pixel = np.reshape(I_pixel_flat, (y_len+1,x_len+1))
@@ -817,10 +870,18 @@ class mapmaking(object):
                 Ymin = np.amin(np.array([Ymin,np.amin(idxpixel[:, 1])]))
                 Ymax = np.amax(np.array([Ymax,np.amax(idxpixel[:, 1])]))
         
-        finalmap_I_num = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
-        finalmap_Q_num = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
-        finalmap_U_num = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
-        finalmap_den = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_I_est = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_Q_est = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_U_est = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_N_hits = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_c = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_c2 = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_s = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_s2 = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_m = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_I = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_Q = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
+        finalmap_U = np.zeros((int(np.abs(Ymax-Ymin)+1), int(np.abs(Xmax-Xmin)+1)))
 
 <<<<<<< HEAD
             mapvalues = self.map_singledetector(value=self.data[i],sigma=self.weight[i],\
@@ -846,29 +907,9 @@ class mapmaking(object):
             else:
                 idxpixel = self.pixelmap[i].copy()
 
-            # mapvalues = self.map_singledetector(crpix = crpix, value=self.data[i],sigma=1/self.weight[i]**2,\
-            #                                     angle=self.polangle[i], idxpixel = idxpixel)
-
-            (I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, Delta, \
-             A, B, C, D, E, F, param) = self.map_param(crpix=crpix, idxpixel=idxpixel, value=self.data[i], \
-                                                       sigma=1/self.weight[i]**2,angle=self.polangle[i])
-
-            I_num_flat = np.zeros(len(I_est_flat))
-            Q_num_flat = np.zeros(len(Q_est_flat))
-            U_num_flat = np.zeros(len(U_est_flat))
-            den_flat = np.zeros(len(I_est_flat))
-
-
-            index, = np.where(np.abs(Delta)>0.)
-            
-            I_num_flat[index] = (A[index]*I_est_flat[index]+B[index]*Q_est_flat[index]+\
-                                 C[index]*U_est_flat[index])
-            Q_num_flat[index] = (B[index]*I_est_flat[index]+D[index]*Q_est_flat[index]+\
-                                 E[index]*U_est_flat[index])
-            U_num_flat[index] = (C[index]*I_est_flat[index]+E[index]*Q_est_flat[index]+\
-                                 F[index]*U_est_flat[index])
-
-            den_flat[index] = Delta[index]
+            (I_est_flat, Q_est_flat, U_est_flat, N_hits_flat, \
+             c_flat, c2_flat, s_flat, s2_flat, m_flat, param) = self.map_param(crpix=crpix, idxpixel=idxpixel, value=self.data[i], \
+                                                                               noise=1/self.weight[i],angle=self.polangle[i])
 
             Xmin_map_temp, Xmax_map_temp = np.amin(idxpixel[:,0]), np.amax(idxpixel[:,0])
             Ymin_map_temp, Ymax_map_temp = np.amin(idxpixel[:,1]), np.amax(idxpixel[:,1])
@@ -881,33 +922,68 @@ class mapmaking(object):
             x_len = Xmax_map_temp-Xmin_map_temp
             y_len = Ymax_map_temp-Ymin_map_temp
 
-            if len(I_num_flat) < (x_len+1)*(y_len+1):
+            if len(I_est_flat) < (x_len+1)*(y_len+1):
                 valmax = (x_len+1)*(y_len+1)
                 pmax = np.amax(param)
                 I_num_fin = 0.*np.arange(pmax+1, valmax)
                 Q_num_fin = 0.*np.arange(pmax+1, valmax)
                 U_num_fin = 0.*np.arange(pmax+1, valmax)
-                den_fin = np.ones(np.abs(pmax+1-valmax))
+                N_hits_fin = 0.*np.arange(pmax+1, valmax)
+                c_fin = 0.*np.arange(pmax+1, valmax)
+                c2_fin = 0.*np.arange(pmax+1, valmax)
+                s_fin = 0.*np.arange(pmax+1, valmax)
+                s2_fin = 0.*np.arange(pmax+1, valmax)
+                m_fin = 0.*np.arange(pmax+1, valmax)
                 
-                I_num_flat = np.append(I_num_flat, I_num_fin)
-                Q_num_flat = np.append(Q_num_flat, Q_num_fin)
-                U_num_flat = np.append(U_num_flat, U_num_fin)
-                den_flat = np.append(den_flat, den_fin)
+                I_est_flat = np.append(I_est_flat, I_num_fin)
+                Q_est_flat = np.append(Q_est_flat, Q_num_fin)
+                U_est_flat = np.append(U_est_flat, U_num_fin)
+                N_hits_flat = np.append(N_hits_flat, N_hits_fin)
+                c_flat = np.append(c_flat, c_fin)
+                c2_flat = np.append(c2_flat, c2_fin)
+                s_flat = np.append(s_flat, s_fin)
+                s2_flat = np.append(s2_flat, s2_fin)
+                m_flat = np.append(m_flat, m_fin)
+
+            I_est = np.reshape(I_est_flat, (y_len+1,x_len+1))
+            Q_est = np.reshape(Q_est_flat, (y_len+1,x_len+1))
+            U_est = np.reshape(U_est_flat, (y_len+1,x_len+1))
+            N_hits_est = np.reshape(N_hits_flat, (y_len+1,x_len+1))
+            c_est = np.reshape(c_flat, (y_len+1,x_len+1))
+            c2_est = np.reshape(c2_flat, (y_len+1,x_len+1))
+            s_est = np.reshape(s_flat, (y_len+1,x_len+1))
+            s2_est = np.reshape(s2_flat, (y_len+1,x_len+1))
+            m_est = np.reshape(m_flat, (y_len+1,x_len+1))
 
 
-            I_temp_num = np.reshape(I_num_flat, (y_len+1,x_len+1))
-            Q_temp_num = np.reshape(Q_num_flat, (y_len+1,x_len+1))
-            U_temp_num = np.reshape(U_num_flat, (y_len+1,x_len+1))
-            temp_den = np.reshape(den_flat, (y_len+1,x_len+1))
+            finalmap_I_est[index1y:index2y+1,index1x:index2x+1] += I_est
+            finalmap_Q_est[index1y:index2y+1,index1x:index2x+1] += Q_est
+            finalmap_U_est[index1y:index2y+1,index1x:index2x+1] += U_est
+            finalmap_N_hits[index1y:index2y+1,index1x:index2x+1] += N_hits_est
+            finalmap_c[index1y:index2y+1,index1x:index2x+1] += c_est
+            finalmap_c2[index1y:index2y+1,index1x:index2x+1] += c2_est
+            finalmap_s[index1y:index2y+1,index1x:index2x+1] += s_est
+            finalmap_s2[index1y:index2y+1,index1x:index2x+1] += s2_est
+            finalmap_m[index1y:index2y+1,index1x:index2x+1] += m_est
 
-            finalmap_I_num[index1y:index2y+1,index1x:index2x+1] += I_temp_num
-            finalmap_Q_num[index1y:index2y+1,index1x:index2x+1] += Q_temp_num
-            finalmap_U_num[index1y:index2y+1,index1x:index2x+1] += U_temp_num
-            finalmap_den[index1y:index2y+1,index1x:index2x+1] += temp_den
 
-        finalmap_I = finalmap_I_num/finalmap_den
-        finalmap_Q = finalmap_Q_num/finalmap_den
-        finalmap_U = finalmap_U_num/finalmap_den
+        Delta = (c_flat**2*(c2_flat-N_hits_flat)+2*s_flat*c_flat*m_flat-c2_flat*s_flat**2-\
+                 N_hits_flat*(c2_flat**2+m_flat**2-c2_flat*N_hits_flat))
+        A = -(c2_flat**2+m_flat**2-c2_flat*N_hits_flat)
+        B = c_flat*(c2_flat-N_hits_flat)+s_flat*m_flat
+        C = c_flat*m_flat-s_flat*c2_flat
+        D = -((c2_flat-N_hits_flat)*N_hits_flat+s_flat**2)
+        E = c_flat*s_flat-m_flat*N_hits_flat
+        F = c2_flat*N_hits_flat-c_flat**2
+
+        index, = np.where(np.abs(Delta)>0.)
+        print('INDEX', i, index, np.amin(Delta[index]), np.amax(Delta[index]))
+        finalmap_I[index] = (A[index]*finalmap_I_est[index]+B[index]*finalmap_Q_est[index]+\
+                             C[index]*finalmap_U_est[index])/Delta[index]
+        finalmap_Q[index] = (B[index]*finalmap_I_est[index]+D[index]*finalmap_Q_est[index]+\
+                             E[index]*finalmap_U_est[index])/Delta[index]
+        finalmap_U[index] = (C[index]*finalmap_I_est[index]+E[index]*finalmap_Q_est[index]+\
+                             F[index]*finalmap_U_est[index])/Delta[index]
 
         return finalmap_I, finalmap_Q, finalmap_U
 

src/pointing.py
@@ -1,12 +1,11 @@
 import numpy as np
 import gc
 from astropy import wcs
-import matplotlib.pyplot as plt
 
-class conversion(object):
+class utils(object):
 
     '''
-    class to handle convesion between different coodinates sytem 
+    class to handle conversion between different coodinates sytem 
     '''
 
     def __init__(self, coord1, coord2, lst = None, lat = None):
@@ -51,10 +50,7 @@ class conversion(object):
 
         el = np.arcsin(np.sin(self.coord2)*np.sin(self.lat)+\
                        np.cos(self.lat)*np.cos(self.coord2)*np.cos(hour_angle))
-        # x = -np.sin(self.lat)*np.cos(self.coord2)*np.cos(hour_angle) + np.cos(self.lat)*np.sin(self.coord2)
-        # y = np.cos(self.coord2)*np.sin(hour_angle)
 
-        # az = np.arctan2(y, x)
         az = np.arccos((np.sin(self.coord2)-np.sin(self.lat)*np.sin(el))/(np.cos(self.lat)*np.cos(el)))
 
         if isinstance(az, np.ndarray):
@@ -74,20 +70,11 @@ class conversion(object):
         dec = np.arcsin(np.sin(self.coord2)*np.sin(self.lat)+\
                         np.cos(self.lat)*np.cos(self.coord2)*np.cos(np.radians(self.coord1)))
 
-        # x = -np.sin(self.lat)*np.cos(self.coord2)*np.cos(np.radians(self.coord1)) +\
-        #     np.cos(self.lat)*np.sin(self.coord2)
-
-        # y = -np.cos(self.coord2)*np.sin(np.radians(self.coord1))
-
-        # hour_angle = np.arctan2(y, x)
 
         hour_angle = np.arccos((np.sin(self.coord2)-np.sin(self.lat)*np.sin(dec))/(np.cos(self.lat)*np.cos(dec)))
 
         index, = np.where(np.sin(np.radians(self.coord1)) > 0)
         hour_angle[index] = 2*np.pi - hour_angle[index]
-
-        # index, = np.where(hour_angle<0)
-        # hour_angle += 2.*np.pi
 
         ra = self.ha2ra(np.degrees(hour_angle)/15.)*15.
 
@@ -95,6 +82,59 @@ class conversion(object):
         ra[index] += 360.
 
         return ra, np.degrees(dec)
+
+    def parallactic_angle(self):
+
+        '''
+        Compute the parallactic angle which is returned in radians
+        '''
+
+        hour_angle = np.radians((self.ra2ha())*15)
+
+        if isinstance(hour_angle, np.ndarray):
+            try:
+                index, = np.where(hour_angle<0)
+                hour_angle[index] += 2*np.pi
+            except ValueError:
+                index, = np.where(hour_angle[0]<0)
+                hour_angle[0,index] += 2*np.pi
+        else:
+            if hour_angle<=0:
+                hour_angle += 2*np.pi
+
+        y_pa = np.cos(self.lat)*np.sin(hour_angle)
+        x_pa = np.sin(self.lat)*np.cos(self.coord2)-np.cos(hour_angle)*np.cos(self.lat)*np.sin(self.coord2)
+
+        pa = np.arctan2(y_pa, x_pa)
+
+        return np.degrees(pa)
+
+class convert_to_telescope(object):
+
+    '''
+    Class to convert from sky equatorial coordinates to telescope coordinates
+    '''
+
+    def __init__(self, coord1, coord2, lst, lat):
+
+        self.coord1 = coord1           #RA, needs to be in hours       
+        self.coord2 = coord2           #DEC
+        self.lst = lst 
+        self.lat = lat
+
+    def conversion(self):
+
+        '''
+        This function rotates the coordinates projected on the plane using the parallactic angle
+        '''
+        
+        parang = utils(self.coord1, self.coord2, self.lst, self.lat)
+        pa = parang.parallactic_angle()
+
+        x_tel = np.radians(self.coord1*15)*np.cos(pa)-np.radians(self.coord2)*np.sin(pa)
+        y_tel = np.radians(self.coord2)*np.cos(pa)+np.radians(self.coord1*15)*np.sin(pa)
+
+        return np.degrees(x_tel), np.degrees(y_tel)
 
 class apply_offset(object):
 
@@ -117,12 +157,11 @@ class apply_offset(object):
 
         if self.ctype.lower() == 'ra and dec':
 
-            conv2azel = conversion(self.coord1, self.coord2, self.lst, self.lat)
+            conv2azel = utils(self.coord1, self.coord2, self.lst, self.lat)
 
             az, el = conv2azel.radec2azel()
 
             xEL = np.degrees(np.radians(az)*np.cos(np.radians(el)))
-            print('STARCAMER', self.xsc_offset)
             xEL_corrected = xEL-self.xsc_offset[0]
             EL_corrected = el+self.xsc_offset[1]
             
@@ -130,69 +169,37 @@ class apply_offset(object):
             dec_corrected = np.zeros((int(np.size(self.det_offset)/2), len(xEL_corrected)))
 
             for i in range(int(np.size(self.det_offset)/2)):
-                print('DET_OFF', self.det_offset[i])
+
                 xEL_corrected_temp = xEL_corrected-self.det_offset[i, 0]
                 EL_corrected_temp = EL_corrected+self.det_offset[i, 1]
                 AZ_corrected_temp = np.degrees(np.radians(xEL_corrected_temp)/np.cos(np.radians(el)))
 
-                conv2radec = conversion(AZ_corrected_temp, EL_corrected_temp, \
-                                        self.lst, self.lat)
+                conv2radec = utils(AZ_corrected_temp, EL_corrected_temp, \
+                                   self.lst, self.lat)
 
                 ra_corrected[i,:], dec_corrected[i,:] = conv2radec.azel2radec()
 
-                hour_angle = np.radians((self.lst-self.coord1)*15)
-                print('hour', hour_angle)
-                index, = np.where(hour_angle<0)
-                hour_angle[index] += 2*np.pi
+                # hour_angle = np.radians((self.lst-self.coord1)*15)
+                # print('hour', hour_angle)
+                # index, = np.where(hour_angle<0)
+                # hour_angle[index] += 2*np.pi
 
-                y_pa = np.cos(np.radians(self.lat))*np.sin(hour_angle)
-                x_pa = np.sin(np.radians(self.lat))*np.cos(np.radians(self.coord2))-np.cos(hour_angle)*np.cos(np.radians(self.lat))*np.sin(np.radians(self.coord2))
-                pa = np.arctan2(y_pa, x_pa)
-
-                if isinstance(pa, np.ndarray):
-                    index, = np.where(pa<0)
-                    pa[index] += 2*np.pi
-                else:
-                    if pa <= 0:
-                        pa += 2*np.pi
+                # y_pa = np.cos(np.radians(self.lat))*np.sin(hour_angle)
+                # x_pa = np.sin(np.radians(self.lat))*np.cos(np.radians(self.coord2))-np.cos(hour_angle)*np.cos(np.radians(self.lat))*np.sin(np.radians(self.coord2))
+                # pa = np.arctan2(y_pa, x_pa)
                 
-                dec_corr= self.coord2+np.degrees(-np.radians(self.det_offset[i,0])*np.sin(pa)+np.radians(self.det_offset[i,1])*np.cos(pa))
-                ra_corr = self.coord1*15+np.degrees((np.radians(self.det_offset[i,0])*np.cos(pa)+\
-                          np.radians(self.det_offset[i,1])*np.sin(pa))/np.cos(np.radians(dec_corrected[i,:])))
-
-                plt.figure(i)
-                #plt.plot(dec_corr, 'o')
-                plt.plot(dec_corrected[i,:]-dec_corr)
-
-                # print('min', np.amin(dec_corr), np.amin(np.degrees(ra_corr)))
-
-                plt.figure(i+1)
-                #plt.plot(np.degrees(ra_corr), 'o')
-                plt.plot(ra_corrected[i,:]-ra_corr)
-                # plt.plot(self.coord1*15, label = 'original')
-                # plt.legend()
-
-                plt.show()
-
-                # # plt.figure(i+2)
-                # # plt.plot((np.radians(self.det_offset[i,0])*np.cos(pa)+\
-                # #           np.radians(self.det_offset[i,1])*np.sin(pa)))
-
-                # plt.show()
+                # dec_corrected[i,:]= self.coord2+np.degrees(-np.radians(self.det_offset[i,0])*np.sin(pa)+np.radians(self.det_offset[i,1])*np.cos(pa))
+                # ra_corrected[i,:] = self.coord1*15+np.degrees((np.radians(self.det_offset[i,0])*np.cos(pa)+\
+                #           np.radians(self.det_offset[i,1])*np.sin(pa))/np.cos(np.radians(dec_corrected[i,:])))
 
             del xEL_corrected_temp
             del EL_corrected_temp
             del AZ_corrected_temp
             gc.collect()
 
-            # plt.plot(self.coord1*15, label = 'original')
-            
-            # plt.legend()
-            # plt.show()
-
             return ra_corrected, dec_corrected
 
-        elif self.cype.lower() == 'az and el':
+        elif self.ctype.lower() == 'az and el':
 
             el_corrected = np.zeros((int(np.size(self.det_offset)/2), len(self.coord1)))
             az_corrected = np.zeros((int(np.size(self.det_offset)/2), len(self.coord2)))
@@ -218,7 +225,6 @@ class apply_offset(object):
 
 
             return xel_corrected,el_corrected
-
 
 class compute_offset(object):
 
@@ -269,10 +275,6 @@ class compute_offset(object):
         x_c = np.sum(xx*weight*self.map_data)/flux
         y_c = np.sum(yy*weight*self.map_data)/flux
 
-        plt.imshow(self.map_data, origin = 'lower', extent=[x_coord_min, x_coord_max, y_coord_min, y_coord_max])
-        plt.plot(x_c, y_c, 'x')
-        plt.show()
-
         return np.rint(x_c), np.rint(y_c)
     
     def value(self):
@@ -287,7 +289,7 @@ class compute_offset(object):
             x_map = map_center.ra.hour
             y_map = map_center.dec.degree
             print('b1', x_c, y_c, x_map*15., y_map, np.average(self.lst), np.average(self.lat))
-            centroid_conv = conversion(x_map, y_map, np.average(self.lst), np.average(self.lat))
+            centroid_conv = utils(x_map, y_map, np.average(self.lst), np.average(self.lat))
 
             coord1_reference = self.coord1_ref/15.
 
@@ -304,14 +306,12 @@ class compute_offset(object):
                 xel_centr = x_map/np.cos(np.radians(el_centr))
             
 
-        ref_conv = conversion(coord1_reference, self.coord2_ref, np.average(self.lst), \
-                              np.average(self.lat))
+        ref_conv = utils(coord1_reference, self.coord2_ref, np.average(self.lst), \
+                         np.average(self.lat))
 
         az_ref, el_ref = ref_conv.radec2azel()
 
         xel_ref = az_ref*np.cos(np.radians(el_ref))
-
-        print('TEST_REF')
 
         return xel_centr-xel_ref, el_ref-el_centr
 