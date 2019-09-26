Correct telescope coordinates calculation

src/beam.py
@@ -3,10 +3,6 @@ from scipy.linalg import svd
 from scipy.optimize import least_squares
 from photutils import find_peaks
 from astropy.stats import sigma_clipped_stats
-from astropy import wcs, coordinates
-
-#import matplotlib.pyplot as plt
-
 
 class beam(object):
 
@@ -94,22 +90,18 @@ class beam(object):
                                     np.ones(len(np.ravel(self.data))), np.amax(self.data)), \
                               method='lm')
             
-            # J = p.jac
-            # cov = np.linalg.inv(J.T.dot(J))
-            # var = np.sqrt(np.diagonal(cov))
-
             _, s, VT = svd(p.jac, full_matrices=False)
             threshold = np.finfo(float).eps * max(p.jac.shape) * s[0]
             s = s[s > threshold]
             VT = VT[:s.size]
             var = np.dot(VT.T / s**2, VT)
-            print('VAR', np.sqrt(np.diag(var)))
             return p, var
         except np.linalg.LinAlgError:
             msg = 'Fit not converged'
             return msg, 0
-        # except ValueError:
-        #     msg = 'Too Many '
+        except ValueError:
+            msg = 'Too Many parameters',
+            return msg, 0
 
     def beam_fit(self, mask_pf= False):
 

src/detTOD.py
@@ -10,12 +10,14 @@ class data_cleaned():
     the next classes. Check them for more explanations
     '''
 
-    def __init__(self, data, fs, cutoff, detlist):
+    def __init__(self, data, fs, cutoff, detlist, polynomialorder, sigma):
 
         self.data = data                #detector TOD
         self.fs = float(fs)             #frequency sampling of the detector
         self.cutoff = float(cutoff)     #cutoff frequency of the highpass filter
         self.detlist = detlist          #detector name list
+        self.polynomialorder = polynomialorder #polynomial order for fitting
+        self.sigma = sigma #polynomial order for fitting
 
     def data_clean(self):
 
@@ -27,10 +29,13 @@ class data_cleaned():
 
         if np.size(self.detlist) == 1:
             det_data = detector(self.data, 0, 0)
-            residual_data = det_data.fit_residual()
+            if self.polynomialorder != 0:
+                residual_data = det_data.fit_residual(order=self.polynomialorder)
+            else:
+                residual_data = det_data.copy()
 
             desp = despike(residual_data)
-            data_despiked = desp.replace_peak()
+            data_despiked = desp.replace_peak(hthres=self.sigma)
 
             filterdat = filterdata(data_despiked, self.cutoff, self.fs)
             cleaned_data = filterdat.ifft_filter(window=True)

src/test/pointing.py
@@ -70,12 +70,12 @@ y_pa = np.cos(latf)*np.sin(hour_angle)
 x_pa = np.sin(latf)*np.cos(decf) - np.cos(hour_angle)*np.cos(latf)*np.sin(decf)
 pa = np.arctan2(y_pa, x_pa)
 
-if isinstance(pa, np.ndarray):
-    index, = np.where(pa<0)
-    pa[index] += 2*np.pi
-else:
-    if pa <= 0:
-        pa += 2*np.pi
+# if isinstance(pa, np.ndarray):
+#     index, = np.where(pa<0)
+#     pa[index] += 2*np.pi
+# else:
+#     if pa <= 0:
+#         pa += 2*np.pi
 
 dec_naive = decf-xEL_offset*np.sin(pa)+EL_offset*np.cos(pa)
 ra_naive = np.radians(raf*15.) + (xEL_offset*np.cos(pa)+EL_offset*np.sin(pa))/np.cos(dec_naive)