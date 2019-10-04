3b33ac48e75f7a57b9541428a1aa1f281402ad42
Ian Lowe <ian@modv-ve703-05041.apn.wlan.private.upenn.edu>
2019-10-04 13:13:30 -0400

    added line to allow for no HWPR

--- src/gui.py
+++ src/gui.py
@@ -2006,7 +2006,7 @@ class ParamMapTab(QWidget):
                 if self.HWPCheckBox.isChecked():
                     self.hwp_data = pickle.load(open(hwp_path_pickle, 'rb'))
                 else:
-                    self.hwp_data = 0.
+                    self.hwp_data = None
                 
                 if (correction and self.coord1.lower() == 'ra') or self.telescopecoordinateCheckBox.isChecked():
                     self.lst_data = pickle.load(open(lst_path_pickle, 'rb'))
@@ -2027,16 +2027,24 @@ class ParamMapTab(QWidget):
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             except FileNotFoundError:               
 =======
 >>>>>>> 3f224e8... Added pointing input dialogs and caluclation
                 dataload = ld.data_value(self.detpath.text(), self.detname.text(), self.coordpath.text(), \
 =======
+=======
+                if self.HWPCheckBox.isChecked():
+                    hwptype = self.hwptype.text()
+                else:
+                    hwptype=None
+
+>>>>>>> 38fa5a0... added line to allow for no HWPR
                 dataload = ld.data_value(self.detpath.text(), self.det_list, self.coordpath.text(), \
 >>>>>>> db74452... Solved a bug on applying the offset
                                          self.coord1, self.coord2, self.dettype.text(), \
                                          self.coord1type.text(), self.coord2type.text(), \
-                                         self.experiment.currentText(), lst_file_type, lat_file_type, self.HWPtype.text())
+                                         self.experiment.currentText(), lst_file_type, lat_file_type, hwptype)
 
                 if (correction and self.coord1.lower() == 'ra') or self.telescopecoordinateCheckBox.isChecked():
                     self.det_data, self.coord1_data, self.coord2_data, self.hwp_data, self.lst_data, self.lat_data = dataload.values()
