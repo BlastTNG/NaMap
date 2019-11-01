Corrected calculation of Polarization Angle and bug fix

src/gui.py
@@ -1433,7 +1433,7 @@ class ParamMapTab(QWidget):
 
 
                 if self.HWPCheckBox.isChecked():
-                    hwptype = self.hwptype.text()
+                    hwptype = self.HWPtype.text()
                 else:
                     hwptype=None
 
@@ -1537,10 +1537,9 @@ class ParamMapTab(QWidget):
                 if self.coord1.lower() == 'ra':
                     self.coord1slice = self.coord1slice*15. #Conversion between hours to degree
 
-            if self.telescopecoordinateCheckBox.isChecked():
+            if self.telescopecoordinateCheckBox.isChecked() or self.ICheckBox.isChecked() is False:
                 
                 self.parallactic = np.zeros_like(self.coord1slice)
-                print('PARALLACTIC SHAPE', np.shape(self.parallactic))
                 if np.size(np.shape(self.detslice)) == 1:
                     tel = pt.utils(self.coord1slice/15., self.coord2slice, \
                                    self.lstslice, self.latslice)
@@ -1556,8 +1555,13 @@ class ParamMapTab(QWidget):
                                            self.lstslice, self.latslice)
                             self.parallactic[i,:] = tel.parallactic_angle()
             else:
-                self.parallactic = None
-
+                if np.size(np.shape(self.detslice)) == 1:
+                    self.parallactic = 0.
+                else:
+                    if np.size(np.shape(self.coord1slice)) == 1:
+                        self.parallactic = 0.
+                    else:
+                        self.parallactic = np.zeros_like(self.data)
             del self.coord1_data
             del self.coord2_data 
             del zoomsyncdata
@@ -1649,11 +1653,15 @@ class ParamMapTab(QWidget):
         #Compute final polarization angle
         
         if np.size(self.det_list) == 1:
-            self.pol_angle = np.radians(2*self.hwpslice+(self.grid_angle-2*self.pol_angle_offset))
+            self.pol_angle = np.radians(self.parallactic+2*self.hwpslice+(self.grid_angle-2*self.pol_angle_offset))
         else:
             self.pol_angle = np.zeros_like(data)
             for i in range(np.size(self.det_list)):
                 self.pol_angle[i,:] = np.radians(2*self.hwpslice+(self.grid_angle[i]-2*self.pol_angle_offset[i]))
+                if np.size(np.shape(self.coord1slice)) == 1:
+                    self.pol_angle[i, :] += np.radians(self.parallactic)
+                else:
+                    self.pol_angle[i, :] += np.radians(self.parallactic[i,:])
 
         self.maps = mp.maps(self.ctype, self.crpix, self.cdelt, self.crval, \
                             data, self.coord1slice, self.coord2slice, \

src/mapmaker.py
@@ -37,8 +37,7 @@ class maps():
         '''
         Function to compute the projection and the pixel coordinates
         '''
-        print('IN')
-        wcsworld = wcs_world(self.ctype, self.crpix, self.cdelt, self.crval, self.telcoord, self.parang)
+        wcsworld = wcs_world(self.ctype, self.crpix, self.cdelt, self.crval, self.telcoord)
 
         if np.size(np.shape(self.data)) == 1:
 
@@ -47,16 +46,16 @@ class maps():
             else:
                 coord_array = np.transpose(np.array([self.coord1, self.coord2]))
             try:
-                self.w, self.proj = wcsworld.world(coord_array)
+                self.w, self.proj = wcsworld.world(coord_array, self.parang)
             except RuntimeError:
-                self.w, self.proj = wcsworld.world(coord_array)
+                self.w, self.proj = wcsworld.world(coord_array, self.parang)
         else:
             if np.size(np.shape(self.coord1)) == 1:
-                self.w, self.proj = wcsworld.world(np.transpose(np.array([self.coord1, self.coord2])))
+                self.w, self.proj = wcsworld.world(np.transpose(np.array([self.coord1, self.coord2])), self.parang[i,:])
             else:
                 self.w = np.zeros((np.size(np.shape(self.data)), len(self.coord1[0]), 2))
                 for i in range(np.size(np.shape(self.data))):
-                    self.w[i,:,:], self.proj = wcsworld.world(np.transpose(np.array([self.coord1[i], self.coord2[i]])))
+                    self.w[i,:,:], self.proj = wcsworld.world(np.transpose(np.array([self.coord1[i], self.coord2[i]])), self.parang[i,:])
 
     def map2d(self):
 
@@ -88,7 +87,6 @@ class maps():
         else:
             mapmaker = mapmaking(self.data, self.noise, self.pol_angle, np.size(np.shape(self.data)), np.floor(self.w).astype(int))
             if self.Ionly:
-                print('Multi', np.size(np.shape(self.data)))
                 Imap = mapmaker.map_multidetectors_Ionly(self.crpix)
 
                 if not self.convolution:
@@ -114,16 +112,15 @@ class wcs_world():
     Class to generate a wcs using astropy routines.
     '''
 
-    def __init__(self, ctype, crpix, crdelt, crval, telcoord=False, parang=None):
+    def __init__(self, ctype, crpix, crdelt, crval, telcoord=False):
 
         self.ctype = ctype    #ctype of the map, which projection is used to convert coordinates to pixel numbers
         self.crdelt = crdelt  #cdelt of the map, distance in deg between two close pixels
         self.crpix = crpix    #crpix of the map, central pixel of the map in pixel coordinates
         self.crval = crval    #crval of the map, central pixel of the map in sky/telescope (depending on the system) coordinates
         self.telcoord = telcoord #Telescope coordinates boolean value. Check map class for more explanation
-        self.parang = parang  #Parallactic angle. Check map class for more explanation
 
-    def world(self, coord):
+    def world(self, coord, parang):
         
         '''
         Function for creating a wcs projection and a pixel coordinates 
@@ -149,8 +146,8 @@ class wcs_world():
             world = np.zeros_like(coord)
             px = w.wcs.s2p(coord, 1)
             #Use the parallactic angle to rotate the projected plane
-            world[:,0] = (px['imgcrd'][:,0]*np.cos(self.parang)-px['imgcrd'][:,1]*np.sin(self.parang))/self.crdelt[0]+self.crpix[0]
-            world[:,1] = (px['imgcrd'][:,0]*np.sin(self.parang)+px['imgcrd'][:,1]*np.cos(self.parang))/self.crdelt[1]+self.crpix[1]
+            world[:,0] = (px['imgcrd'][:,0]*np.cos(parang)-px['imgcrd'][:,1]*np.sin(parang))/self.crdelt[0]+self.crpix[0]
+            world[:,1] = (px['imgcrd'][:,0]*np.sin(parang)+px['imgcrd'][:,1]*np.cos(parang))/self.crdelt[1]+self.crpix[1]
         
         print('WORLD', world, np.shape(world))
         return world, w

src/quaternion.py
@@ -17,8 +17,13 @@ class quaternions(object):
         '''
         Function to compute a quaternion from euler angles. Given how yaw, pitch and roll are defined, 
         the quaternion is equivalent to Q=q_yaw*q_pitch*q_roll
+        Input of the function in degrees
         '''
-        
+
+        yaw = np.radians(yaw)
+        roll = np.radians(roll)
+        pitch = np.radians(pitch)
+
         qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
         qx = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
         qy = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)