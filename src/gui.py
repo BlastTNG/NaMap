bdf80032b7e22c129adba4cac599635ad81cae4d
Ian Lowe <ian@Ians-MacBook-Pro-3.local>
2024-04-29 16:00:45 -0700

    fixed misnamed field copy

--- src/gui.py
+++ src/gui.py
@@ -857,7 +857,7 @@ class ParamMapTab(QWidget):
         self.DataRepository = QGroupBox("Data Repository")
         
         #self.detpath = QLineEdit('/mnt/c/Users/gabri/Documents/GitHub/mapmaking/2012_data/bolo_data/')
-        #self.coordpath = QLineEdit('/Users/ian/master_dirfile')
+        #self.detdpath = QLineEdit('/Users/ian/master_dirfile')
         self.detpath = QLineEdit('/mnt/d/data/etc/mole.lnk')
         self.detpathlabel = QLabel("Detector Path:")
         self.detpathlabel.setBuddy(self.detpath)
@@ -873,7 +873,8 @@ class ParamMapTab(QWidget):
         self.roachnumberlabel.setBuddy(self.roachnumber)
 
         #self.coordpath = QLineEdit('/mnt/c/Users/gabri/Documents/GitHub/mapmaking/2012_data/')
-        self.detpath = QLineEdit('/mnt/d/data/etc/mole.lnk')
+        #self.coordpath = QLineEdit('/Users/ian/master_dirfile')
+        self.coordpath = QLineEdit('/mnt/d/data/etc/mole.lnk')
         self.coordpathlabel = QLabel("Coordinate Path:")
         self.coordpathlabel.setBuddy(self.coordpath)
 
